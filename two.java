504679940cb00c04ca48889396dd3186886d7d96
Sean Gates <seanx63@gmail.com>
2023-02-18 18:37:50 -0700

    add Java/2

---
+++ two.java
@@ -0,0 +1,17 @@
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+class two {
+    public static void main(String[] args) {
+        int prev = 0;
+        int curr = 1;
+        int sum = 0;
+        while (curr < 4000000) {
+            if (curr % 2 == 0) {
+                sum += curr;
+            }
+            int temp = curr;
+            curr += prev;
+            prev = temp;
+        }
+        System.out.println(sum); // 4613732
+    }
+}
