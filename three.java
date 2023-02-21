c32d696f9205d0f0318fd213ca64bb18d408bb44
Sean Gates <seanx63@gmail.com>
2023-02-21 10:23:42 -0700

    add Java/3

---
+++ three.java
@@ -0,0 +1,30 @@
+// What is the largest prime factor of the number 600851475143?
+class three {
+	public static void main(String[] args) {
+			System.out.println(largestPrimeFactor(600851475143L)); // 6857
+	}
+	public static long largestPrimeFactor(long x) {
+		if (x <= 1 || x == 2 || x == 3) {
+			return x;
+		}
+		for (long i = 2; i < x/2; i++) {
+			if (x % i == 0) {
+				if (isPrime(x/i)) {
+					return x/i;
+				}
+			}
+		}
+		return x;
+	}
+	public static Boolean isPrime(long x) {
+		if (x <= 1 || x % 2 == 0) {
+			return false;
+		}
+		for (long i = 2; i < x/2; i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
